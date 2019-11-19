0ff60f147e2e152eedd5208f834cc93d2a615d18
shailesh <shailesh.sridhar@gmail.com>
2019-11-19 23:27:31 +0530

    updated recommender system serving code

--- recommender-system/app.py
+++ recommender-system/app.py
@@ -8,23 +8,45 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
 mongo = PyMongo(app)
 
+#API SPEC################
+'''
+user_actions:
+UID, PROPID, ACTION(int)
+'''
+
+def is_higher_priority_action(curr_action, old_action):
+#    action_priority_dict = {"add_to_cart": 3, "click": 2, "hower": 1}
+#    curr_priority = action_priority_dict[curr_action]
+#    old_priority = action_priority_dict[old_action]
+#    return curr_priority > old_priority 
+    return curr_action>old_action
+
 @app.route("/user_actions", methods=['POST'])
 def user_actions():
     user_actions = mongo.db.user_actions
+#    req = request.get_json()
     user = request.json['user']
     property_id = request.json['property_id']
     action = request.json['action']
-    user_actions_id = user_actions.insert({'user': user, 'property_id': property_id, 'action': action})
-    new_user_action = user_actions.find_one({'_id': user_actions_id })
+    relevant_entry = user_actions.find_one({'user': user, 'property_id': property_id})
+#    print(relevant_entry['action'])
+    if relevant_entry == None:
+        user_actions_id = user_actions.insert({'user': user, 'property_id': property_id, 'action': action})
+    elif is_higher_priority_action(action, relevant_entry['action']):
+        user_actions.delete_one(relevant_entry)
+        relevant_entry = user_actions.insert({'user': user, 'property_id': property_id, 'action': action})
+#        relevant_entry.save()
+#    new_user_action = user_actions.find_one({'_id': user_actions_id })
     return "Whatever"
 
-@app.route("/user_recommendations/<user>", methods=['GET'])
+@app.route("/user_recommendations/<user>", methods=['POST'])
 def user_recommendations(user):
     user_actions = mongo.db.user_actions
-    output = []
-    for s in user_actions.find():
-        output.append({'user' : s['user'], 'property_id' : s['property_id'], 'action': s['action']})
-    df = rs.makeDfFromData(output)
+    tr_data = []
+    output = request.json['property_list']
+    for s in user_actions.find({'user':user}):
+        tr_data.append({'user' : s['user'], 'property_id' : s['property_id'], 'action': s['action']})
+    df = rs.makeDfFromData(tr_data)
     model = rs.trainModel(df)
     output = rs.outputTopK(model,user,output,len(output))
     return jsonify({'result' : output})
@@ -35,4 +57,4 @@ def user_recommendations(user):
 #    u
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
